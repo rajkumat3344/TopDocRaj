4a9b058d2f28cd77071451f0d146a15e60cac15c
Gagunsarthak <saisarthakmohanty1996@gmail.com>
2022-07-29 19:08:41 +0530

    pseudo controller for search

--- routes/search/controller.js
+++ routes/search/controller.js
@@ -1,8 +1,45 @@
 
 
+function generateFilterStructure(params, key, value) {
+    try {
+        params['filter' + key] = true
+        params['filter' + key + 'Value'] = value
+    } catch (error) {
+        log.error('error', error)
+        throw error.toString()
+    }
+    return params;
+}
 async function getSearchDetails(body){
     // console.log("Akash bhai")
-    // console.log(body)
+     console.log(body)
+    try{
+        let esType = "searchType"
+        let esIndex = "searchIndex"
+        let esTemplate = "doctorTemplate"
+        let params = {}
+        params.from = body.pageNo * body.pageSize
+        params.size = body.pageSize
+
+
+
+          //generating sort filters
+          if (request.hasOwnProperty('sort') == true && Object.keys(request.sort.length > 0)) {
+            params.sort = true
+            params.sortValue = request.sort
+        }
+        //processing of Filters
+        if (request.hasOwnProperty('filters') == true && Object.keys(request.filters.length > 0)) {
+            //console.log("in if")
+            for (var key in request.filters) {
+                //console.log("key : ",key)
+                if(request.filters[key].length >0){
+                    params = generateFilterStructure(params,key,request.filters[key])
+                }
+            }
+        }
+
+    }catch(     err){}
     
     
 }
