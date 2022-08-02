cf684800942696fa5ecad771f96e42d5b60c78c1
Gagunsarthak <saisarthakmohanty1996@gmail.com>
2022-08-02 20:23:16 +0530

    pulling changes

--- routes/doctors/router.js
+++ routes/doctors/router.js
@@ -12,21 +12,8 @@ cloudinary.config({
 
 //get Profile details (all & specific Field search)
 function getProfileDetails(req, res) {
-  //req.header("Access-Control-Allow-Origin","*")
-  // // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-  // // Request methods you wish to allow
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // // Set to true if you need the website to include cookies in the requests sent
-  // // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-  
-
+ // req.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+ 
   let doctorId;
   let fieldsToFetch;
   let role;
